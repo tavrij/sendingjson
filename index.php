ee4c778e7139dfb94f7bfbd7f2d1eb962bd24855
Amir <Tavrij@gmail.com>
2017-01-24 02:19:15 +0330

    no message

--- index.php
+++ index.php
@@ -1,9 +1,39 @@
 <?php
-    $myarray[] = 500;
-    $myarray[] = "hello world";
-    $myjson = json_encode($myarray);
+
+$data = '    { "user" : [   
+                 { "firstName" : "Vignesh",  
+                   "lastName"  : "Prajapati",
+                   "age"       : 23,
+                   "email"     : ["vignesh@gmail.com","vignesh@yahoo.com"],
+                   "subject"   : ["English","Gujarati", "Hindi"]
+                 }, 
+
+                 { "firstName" : "Vaibhav",  
+                   "lastName"  : "Prajapati",
+                   "age"       : 19,
+                   "email"     : ["vaibhav@gmail.com","vaibhav@yahoo.com","vaibhav@aol.com"],
+                   "subject"   : ["English","Spanish", "Chinese","Sanskrit"]
+                 }
+               ]
+    } ';
+
+$url_send ="https://sendingjson.herokuapp.com/post.php";
+$str_data = json_encode($data);
+
+function sendPostData($url, $post){
+ $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");  
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($post))                                                                       
+   );  
+  $result = curl_exec($ch);
+  curl_close($ch);  // Seems like good practice
+  return $result;
+}
+sendPostData($url_send,$str_data);
+echo "ok";
 ?>
-<form name="input" action="post.php">
-    <input type="hidden" name="json" value="<?php echo $myjson ?>" />
-    <input type="submit" value="Submit">
-</form>
--- post.php
+++ post.php
@@ -1,7 +1,8 @@
 <?php
-     $posted_data = array();
-    if (!empty($_POST['json'])) {
-        $posted_data = json_decode($_POST['json'], true);
-    }
-    print_r($posted_data);
+
+$json_input_data=json_decode(file_get_contents('php://input'),TRUE);
+
+print_r( $json_input_data);
+
+
 ?>
