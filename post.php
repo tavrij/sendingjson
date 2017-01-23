002fa68f491b405cf83592c9391ec611cbeea831
Amir <Tavrij@gmail.com>
2017-01-24 02:00:28 +0330

    no message

--- post.php
+++ post.php
@@ -6,6 +6,6 @@
     print_r($posted_data); */
     $data = json_decode($_POST['json'], true);
 
-    print_r $data[0]; // Should output 500
+    echo $data[0]; // Should output 500
     print_r $data[1]; // Should output hello world	
 ?>
