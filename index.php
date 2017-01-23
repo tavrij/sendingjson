df3897f6159df4bb91d0b7db1f9cab6b3c0f0bfa
Amir <Tavrij@gmail.com>
2017-01-24 01:31:56 +0330

    no message

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     $myjson = json_encode($myarray);
 ?>
 <form name="input" action="post.php">
-    <input type="hidden" name="json" value="<?php echo $myjson ?>" />
+    <input type="hidden" name="json" value="<?php echo $myarray ?>" />
     <input type="submit" value="Submit">
 </form>
