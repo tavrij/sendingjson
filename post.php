e8f56a46f095452cef5be385580db9ffb811768a
Amir <Tavrij@gmail.com>
2017-01-24 01:44:18 +0330

    no message

--- post.php
+++ post.php
@@ -1,7 +1,11 @@
 <?php
-    $posted_data = array();
+/*     $posted_data = array();
     if (!empty($_POST['json'])) {
         $posted_data = json_decode($_POST['json'], true);
     }
-    print_r($posted_data);
+    print_r($posted_data); */
+    $data = json_decode($_REQUEST['json']);
+
+    echo $data[0]; // Should output 500
+    echo $data[1]; // Should output hello world	
 ?>
