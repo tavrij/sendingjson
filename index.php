c9ec5cba1d39f6bc25e4af44cf47493bd76d7ce1
Amir <Tavrij@gmail.com>
2017-01-24 01:26:34 +0330

    no message

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-
+    $myarray[] = 500;
+    $myarray[] = "hello world";
+    $myjson = json_encode($myarray);
 ?>
+<form name="input" action="post.php">
+    <input type="hidden" name="json" value="<?php echo $myjson ?>" />
+    <input type="submit" value="Submit">
+</form>
---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+    $posted_data = array();
+    if (!empty($_POST['json'])) {
+        $posted_data = json_decode($_POST['json'], true);
+    }
+    print_r($posted_data);
+?>
