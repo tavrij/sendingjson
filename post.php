59bc078011592c23bc4f0ef3915f704839b2770f
Amir <Tavrij@gmail.com>
2017-01-24 02:03:37 +0330

    te

--- post.php
+++ post.php
@@ -1,11 +1,7 @@
 <?php
-/*     $posted_data = array();
+     $posted_data = array();
     if (!empty($_POST['json'])) {
         $posted_data = json_decode($_POST['json'], true);
     }
-    print_r($posted_data); */
-    $data = json_decode($_POST['json'], true);
-
-    echo $data[0]; // Should output 500
-    print_r $data[1]; // Should output hello world	
+    print_r($posted_data);
 ?>
